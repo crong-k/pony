491007cd2a5e3d4da02adecea56cac9171a15d73
Your Name <ubuntu@ip-172-31-29-149.ap-northeast-2.compute.internal>
2019-04-07 04:10:26 +0000

    ---

---
+++ flask-backend/main.py
@@ -0,0 +1,17 @@
+import flask
+
+
+app = flask.Flask("__main__")
+
+@app.route("/")
+def my_index():
+    return flask.render_template("index.html",token="hello sy")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
+~
+~
+~
+~
+
