4bce00b76502d9d9f0ba82d09e925ca7b986782f
crong-k <crong24601@gmail.com>
2019-04-07 04:48:26 +0000

    ---

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -1,17 +1,8 @@
 import flask
-
-
 app = flask.Flask("__main__")
-
 @app.route("/")
 def my_index():
     return flask.render_template("index.html",token="hello sy")
 
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
-~
-~
-~
-~
-
---
+++ flask-backend/static/react/precache-manifest.8cfdf4b24b6f7ee12a217962ceb89a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "28475530b879b655205d",
+    "url": "/static/react/js/main.b118f175.chunk.js"
+  },
+  {
+    "revision": "690d085228ad8d2d3a04",
+    "url": "/static/react/js/2.42dc992a.chunk.js"
+  },
+  {
+    "revision": "28475530b879b655205d",
+    "url": "/static/react/css/main.f7ddd581.chunk.css"
+  },
+  {
+    "revision": "690d085228ad8d2d3a04",
+    "url": "/static/react/css/2.84aeacdb.chunk.css"
+  },
+  {
+    "revision": "f3e5402cdaf2f44ac095596104574db3",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
